6220b0bb7202cec2b4a7ce7d4905f420526d71a7
nataliacarvalhoreis <nataliacarvalhoreis@gmail.com>
2022-12-23 13:21:17 -0300

    Unchecked Exceptions

--- exceptions-java/src/br/com/dio/exceptions/UncheckedException.java
+++ exceptions-java/src/br/com/dio/exceptions/UncheckedException.java
@@ -4,17 +4,26 @@ import javax.swing.JOptionPane;
 
 public class UncheckedException {
     public static void main(String[] args) {
-        String a = JOptionPane.showInputDialog("Numerador: ");
-        String b = JOptionPane.showInputDialog("Denominador");
 
-        try {
-            int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
-            System.out.println("Resultado: " + resultado);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("Chegou no finally!");
-        }
+        boolean continueLooping = true;
+
+        do {
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador");
+
+            try {
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+                System.out.println("Resultado: " + resultado);
+                continueLooping = false;
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Entrada inválida, informe um número inteiro!" + e.getMessage());
+            } catch (ArithmeticException e) {
+                JOptionPane.showMessageDialog(null, "Erro! Divisão por 0 não é válida. ");
+            } finally {
+                System.out.println("Chegou no finally!");
+            }
+        } while (continueLooping);
 
         System.out.println("O código continua...");
 
